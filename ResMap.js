94ac3ebe521834d399d6e4af668c4796dd98ba2a
JulieZhang0102 <jzhang10@babson.edu>
2021-12-16 19:16:33 -0500

    Update ResMap.js

--- ResMap.js
+++ ResMap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Callout, MapView, Marker} from "react-native-maps";
+import { MapView, Marker, Callout} from "react-native-maps";
 
 export function RestMapScreen({ navigation, route }) {
 
